f19e1644e563ef8b6b3fcaf3ac7b56c0ae90f8b7
bablysingh2409 <97339369+bablysingh2409@users.noreply.github.com>
2024-04-19 18:23:37 +0530

    .

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://localhost:27017/email-collection", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 
 app.use("/", emailSend);
 
